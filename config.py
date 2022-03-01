10b626c84c55f286553bba386ab9ef455a95c7e4
Alialavi33 <100292616+Alialavi33@users.noreply.github.com>
2022-03-02 00:32:25 +0330

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ if HEROKU:
     from dotenv import load_dotenv
 
     load_dotenv()  # take environment variables from .env.
-    API_ID = int(environ["API_ID"])
-    API_HASH = environ["API_HASH"]
+    API_ID = int(environ["14466322"])
+    API_HASH = environ["7a8af273b6f112a2e218d2e8eb431580"]
     SESSION_STRING = environ[
-        "SESSION_STRING"
+        "BAC-RMMOgyFHmTPUHx5PaV4yLrI3oN9MpvUkhKB8QrsmreUWvfQsfi2Xvkj9w-10FzU5NDXvtbo35ch-pkAfTDp0Jo7oKhJNQIju2WBI87uiNFqwmtS9_GahvDpu83wYMiQnVQPrNxzZsanDr8duMV3U52v8y0EWCX3k-JX3ekDKxvRoOm0L9v8STvZgym8AiSUjzio1dvvunNXswUHXbJU1rTDjFhVbCs9lDdvzyfZia01RwHrHa4pJfsKeVkX1BduRPfxlNrwdXONzFlWFMKMEQdKwp_Z56nls6hWjBd6hwLrUyttlhyUiAqDOmIeXScocnBNvRtvtaRnmaAgdecAAAAATQi0i0A"
     ]  # Check Readme for session
-    ARQ_API_KEY = environ["ARQ_API_KEY"]
-    CHAT_ID = int(environ["CHAT_ID"])
+    ARQ_API_KEY = environ["ILZPLM-WQHUAH-ABJZZX-SNLYWW-ARQ"]
+    CHAT_ID = int(environ["-1001778673594"])
     DEFAULT_SERVICE = environ.get("DEFAULT_SERVICE") or "youtube"
     BITRATE = int(environ["BITRATE"])
 
